117c67844b9f9eebfbe8cee0a02d0d1108fec72e
saivignesh <saivignesh@mail>
2023-08-22 10:58:46 +0530

    name changes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import First from './components/First';
+import FirstNew from './components/FirstNew';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
   <React.StrictMode>
     {/* <App /> */}
-    <First/>
+    <FirstNew />
   </React.StrictMode>
   </>
 );
